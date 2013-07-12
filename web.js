329fe981884a35e9c8911fe51ae49f12785e8e9e
Ubuntu <ubuntu@ip-10-164-27-185.ec2.internal>
2013-07-12 03:59:23 +0000

    try to update wb.js with buffer r/w

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var newHelloWorld = fs.readFileSync(index.html){
+    
+}
+
+var buf = new Buffer(newHelloWorld);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
